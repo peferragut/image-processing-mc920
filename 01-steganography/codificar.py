6a6811993c71cd4288a8a9624abb06c48db9e2c9
Pedro Ferragut <75221107+peferragut@users.noreply.github.com>
2024-03-31 18:21:31 -0300

    vectorize encoder with numpy methods

--- 01-steganography/codificar.py
+++ 01-steganography/codificar.py
@@ -1,30 +1,27 @@
 import cv2 as cv
+import numpy as np
 import sys
 
 def hide_message(img, bin_text):
-    bin_text += "00000000"
-    data_len = len(bin_text)
-    bit_idx = 0
-    
-    for row in img:
-        for pixel in row:
-            for rgb_word in range(3):
-                if bit_idx == data_len:
-                    return
-                pixel[rgb_word] = pixel[rgb_word] & ~1 | int(bin_text[bit_idx])
-                bit_idx += 1
+    width = img.shape[1]
+    height = img.shape[0]
+    msg_len = len(bin_text)
+    img = np.reshape(img, width * height * 3)
+    img[:msg_len] = img[:msg_len] & ~1 | bin_text
+    img = np.reshape(img, (height, width, 3))
 
 args = len(sys.argv)
 if args != 4:
-    print("Too many arguments!") if args > 4 else print("Not enough arguments!")
+    err = "Too many arguments!" if args > 4 else "Not enough arguments!"
+    print(err)
     print("Arguments required: program file, input image and input text file")
     sys.exit()
 
 img = cv.imread(sys.argv[1])
 
-text = open(sys.argv[2]).read()
-bin_text = ''.join(format(ord(char), '08b') for char in text)
-
+text = open(sys.argv[2]).read() + '\0'
+ascii_text = np.array([ord(char) for char in text], dtype=np.uint8)
+bin_text = np.unpackbits(ascii_text)
 hide_message(img, bin_text)
 
 cv.imwrite(sys.argv[3], img)

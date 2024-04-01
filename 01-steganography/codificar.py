07e2084da44b521b82cb2ffb7ff140a15d03fc2c
Pedro Ferragut <75221107+peferragut@users.noreply.github.com>
2024-03-31 22:20:05 -0300

    improve readability

--- 01-steganography/codificar.py
+++ 01-steganography/codificar.py
@@ -6,7 +6,12 @@ def hide_message(img, bin_text):
     width = img.shape[1]
     height = img.shape[0]
     msg_len = len(bin_text)
-    img = np.reshape(img, width * height * 3)
+    img_len = width * height * 3
+    img = np.reshape(img, img_len)
+
+    if msg_len > img_len:
+        bin_text = bin_text[:img_len]
+
     img[:msg_len] = img[:msg_len] & ~1 | bin_text
     img = np.reshape(img, (height, width, 3))
 
@@ -20,8 +25,8 @@ if args != 4:
 img = cv.imread(sys.argv[1])
 
 text = open(sys.argv[2]).read() + '\0'
-ascii_text = np.array([ord(char) for char in text], dtype=np.uint8)
-bin_text = np.unpackbits(ascii_text)
+ascii_array = np.array([ord(char) for char in text], dtype=np.uint8)
+bin_text = np.unpackbits(ascii_array)
 hide_message(img, bin_text)
 
 cv.imwrite(sys.argv[3], img)

723b3c699d8885c9c257cbe6791fd9d9dca59735
Pedro Ferragut <75221107+peferragut@users.noreply.github.com>
2024-04-02 16:10:10 -0300

    vectorize encoder and decoder to accept arbitrary bit planes

--- 01-steganography/codificar.py
+++ 01-steganography/codificar.py
@@ -1,32 +1,46 @@
+from math import ceil
 import cv2 as cv
 import numpy as np
 import sys
-
-def hide_message(img, bin_text):
+    
+def hide_message(img, bin_text, bit_planes):
     width = img.shape[1]
     height = img.shape[0]
+
+    num_planes = len(bit_planes)
     msg_len = len(bin_text)
+    color_planes = ceil(msg_len / num_planes)
+
     img_len = width * height * 3
     img = np.reshape(img, img_len)
 
-    if msg_len > img_len:
-        bin_text = bin_text[:img_len]
+    max_bits = img_len * num_planes
+    if msg_len > max_bits:
+        bin_text = bin_text[:max_bits]
 
-    img[:msg_len] = img[:msg_len] & ~1 | bin_text
+    mask = np.bitwise_or.reduce(1 << bit_planes)
+    img[:color_planes] = img[:color_planes] & ~mask
+
+    for plane_idx in range(num_planes):
+        text_shifted_bitwise = bin_text[plane_idx::num_planes] << bit_planes[plane_idx]
+        text_shifted_bitwise = np.concatenate((text_shifted_bitwise, np.zeros(color_planes - len(text_shifted_bitwise), dtype=np.uint8)))
+        img[:color_planes] = img[:color_planes] | text_shifted_bitwise
+
     img = np.reshape(img, (height, width, 3))
 
 args = len(sys.argv)
-if args != 4:
-    err = "Too many arguments!" if args > 4 else "Not enough arguments!"
+if args != 5:
+    err = "Too many arguments!" if args > 5 else "Not enough arguments!"
     print(err)
-    print("Arguments required: program file, input image and input text file")
+    print("Arguments required: program file, input image, input text file, bit planes and output image")
     sys.exit()
 
 img = cv.imread(sys.argv[1])
+bit_planes = np.array(list(sys.argv[3]), dtype=np.uint8)[::-1]
 
 text = open(sys.argv[2]).read() + '\0'
 ascii_array = np.array([ord(char) for char in text], dtype=np.uint8)
 bin_text = np.unpackbits(ascii_array)
-hide_message(img, bin_text)
+hide_message(img, bin_text, bit_planes)
 
-cv.imwrite(sys.argv[3], img)
+cv.imwrite(sys.argv[4], img)
--- 01-steganography/decodificar.py
+++ 01-steganography/decodificar.py
@@ -2,12 +2,19 @@ import cv2 as cv
 import numpy as np
 import sys
 
-def decode_msg(img):
+def decode_msg(img, bit_planes):
     width = img.shape[1]
     height = img.shape[0]
     img = np.reshape(img, width * height * 3)
-    px_lsb_array = img & 1
-    packed_bits = np.packbits(px_lsb_array)
+
+    num_planes = len(bit_planes)
+    extracted_planes = []
+    for plane_idx in range(num_planes):
+        extraction_plane = (img >> bit_planes[plane_idx]) & 1
+        extracted_planes.append(extraction_plane)
+    extracted_planes = np.array(extracted_planes)[::-1]
+    bit_planes_img = np.stack(extracted_planes).ravel('F')
+    packed_bits = np.packbits(bit_planes_img)
 
     null_array = np.where(packed_bits == 0)[0]
     delimiter_index = null_array[0] if len(null_array) > 0 else len(packed_bits)
@@ -16,19 +23,21 @@ def decode_msg(img):
     decoded_text = ascii_array.tobytes().decode("ascii")
     return decoded_text
 
-
 args = len(sys.argv)
-if args != 3:
-    err = "Too many arguments!" if args > 3 else "Not enough arguments!"
+if args != 4:
+    err = "Too many arguments!" if args > 4 else "Not enough arguments!"
     print(err)
-    print("Arguments required: program file, output image and output text file")
+    print("Arguments required: program file, output image, bit planes and output text file")
     sys.exit()
 
 img = cv.imread(sys.argv[1])
-decoded_msg = decode_msg(img)
+bit_planes = np.array(list(sys.argv[2]), dtype=np.uint8)
 
-output_file = open(sys.argv[2], 'w')
+decoded_msg = decode_msg(img, bit_planes)
+
+output_file = open(sys.argv[3], 'w')
 output_file.write(decoded_msg)
 output_file.close()
+
 
 

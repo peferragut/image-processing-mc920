2ece9c783ec9e9bc720be4a6e485d9dfbf431f2e
Pedro Ferragut <75221107+peferragut@users.noreply.github.com>
2024-04-16 16:00:54 -0300

    finish coder

--- 01-steganography/codificar.py
+++ 01-steganography/codificar.py
@@ -9,24 +9,28 @@ def hide_message(img, bin_text, bit_planes):
 
     num_planes = len(bit_planes)
     msg_len = len(bin_text)
-    color_planes = ceil(msg_len / num_planes)
 
     img_len = width * height * 3
     img = np.reshape(img, img_len)
 
     max_bits = img_len * num_planes
     if msg_len > max_bits:
-        bin_text = bin_text[:max_bits]
+        msg_len = max_bits
+        bin_text = bin_text[:msg_len]
+        
+    color_planes = ceil(msg_len / num_planes)
 
     mask = np.bitwise_or.reduce(1 << bit_planes)
     img[:color_planes] = img[:color_planes] & ~mask
 
     for plane_idx in range(num_planes):
         text_shifted_bitwise = bin_text[plane_idx::num_planes] << bit_planes[plane_idx]
-        text_shifted_bitwise = np.concatenate((text_shifted_bitwise, np.zeros(color_planes - len(text_shifted_bitwise), dtype=np.uint8)))
+        last_element_extension = np.zeros(color_planes - len(text_shifted_bitwise), dtype=np.uint8)
+        text_shifted_bitwise = np.concatenate((text_shifted_bitwise, last_element_extension))
         img[:color_planes] = img[:color_planes] | text_shifted_bitwise
 
     img = np.reshape(img, (height, width, 3))
+
 
 args = len(sys.argv)
 if args != 5:
@@ -34,9 +38,10 @@ if args != 5:
     print(err)
     print("Arguments required: program file, input image, input text file, bit planes and output image")
     sys.exit()
+    
 
 img = cv.imread(sys.argv[1])
-bit_planes = np.array(list(sys.argv[3]), dtype=np.uint8)[::-1]
+bit_planes = np.array(list(sys.argv[3]), dtype=np.uint8)
 
 text = open(sys.argv[2]).read() + '\0'
 ascii_array = np.array([ord(char) for char in text], dtype=np.uint8)
